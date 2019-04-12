0a07a9bd4d87883a1c0dff21db0b4f3070195ff6
ewangplay <ewangplay@gmail.com>
2019-04-12 18:27:03 +0800

    add gse sample

---
+++ gse-test/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/go-ego/gse"
+)
+
+var (
+	//text = "纽约时代广场, 纽约帝国大厦"
+	text = "北京市海淀区稻香园房产"
+
+	seg gse.Segmenter
+)
+
+func main() {
+	hmm := seg.HMMCutMod(text)
+	fmt.Println("hmm cut: ", hmm)
+
+	seg.LoadDict()
+
+	hmm = seg.Cut(text, true)
+	fmt.Println("hmm cut: ", hmm)
+
+	hmm = seg.CutSearch(text, true)
+	fmt.Println("hmm cut: ", hmm)
+
+	hmm = seg.CutAll(text)
+	fmt.Println("cut all: ", hmm)
+}
