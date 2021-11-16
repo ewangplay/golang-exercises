83ee21a214d5dabec7c6a66177399babf5242a56
Ray Wang <ewangplay@gmail.com>
2021-11-16 16:33:19 +0800

    Update cryptolib sample

--- pkgs/cryptolib/cryptolib.go
+++ pkgs/cryptolib/cryptolib.go
@@ -20,26 +20,38 @@ func main() {
 	}
 
 	fmt.Println("ED25519 test ...")
-	signAndVerify(csp, &cl.ED25519KeyGenOpts{})
+	signAndVerify(csp, &cl.ED25519KeyGenOpts{}, nil)
 
 	fmt.Println("ECDSA with default mode test ...")
-	signAndVerify(csp, &cl.ECDSAKeyGenOpts{})
+	signAndVerify(csp, &cl.ECDSAKeyGenOpts{}, nil)
 
 	fmt.Println("ECDSA with P224 mode test ...")
-	signAndVerify(csp, &cl.ECDSAKeyGenOpts{Curve: elliptic.P224()})
+	signAndVerify(csp, &cl.ECDSAKeyGenOpts{Curve: elliptic.P224()}, nil)
 
 	fmt.Println("ECDSA with P256 mode test ...")
-	signAndVerify(csp, &cl.ECDSAKeyGenOpts{Curve: elliptic.P256()})
+	signAndVerify(csp, &cl.ECDSAKeyGenOpts{Curve: elliptic.P256()}, nil)
 
 	fmt.Println("ECDSA with P384 mode test ...")
-	signAndVerify(csp, &cl.ECDSAKeyGenOpts{Curve: elliptic.P384()})
+	signAndVerify(csp, &cl.ECDSAKeyGenOpts{Curve: elliptic.P384()}, nil)
 
 	fmt.Println("ECDSA with P521 mode test ...")
-	signAndVerify(csp, &cl.ECDSAKeyGenOpts{Curve: elliptic.P521()})
+	signAndVerify(csp, &cl.ECDSAKeyGenOpts{Curve: elliptic.P521()}, nil)
+
+	fmt.Println("RSA with default schema test ...")
+	signAndVerify(csp, &cl.RSAKeyGenOpts{}, nil)
+
+	fmt.Println("RSA with PSS schema test ...")
+	signAndVerify(csp, &cl.RSAKeyGenOpts{Bits: 1024}, &cl.RSASignatureOpts{Schema: cl.PSS})
+
+	fmt.Println("RSA with PKCS1V15 schema test ...")
+	signAndVerify(csp, &cl.RSAKeyGenOpts{Bits: 2048}, &cl.RSASignatureOpts{Schema: cl.PKCS1V15})
+
+	fmt.Println("SM2 with default mode test ...")
+	signAndVerify(csp, &cl.SM2KeyGenOpts{}, nil)
 }
 
-func signAndVerify(csp cl.CSP, opts cl.KeyGenOpts) error {
-	k, err := csp.KeyGen(opts)
+func signAndVerify(csp cl.CSP, keyGenOpts cl.KeyGenOpts, signOpts cl.SignatureOpts) error {
+	k, err := csp.KeyGen(keyGenOpts)
 	if err != nil {
 		fmt.Printf("KeyGen failed: %v\n", err)
 		return err
@@ -48,8 +60,14 @@ func signAndVerify(csp cl.CSP, opts cl.KeyGenOpts) error {
 	kd, _ := k.Bytes()
 	fmt.Println("Private Key: ", hex.EncodeToString(kd))
 
-	digest := []byte("hello,world")
-	signature, err := csp.Sign(k, digest)
+	msg := []byte("hello,world")
+	digest, err := csp.Hash(msg, &cl.SHA256Opts{})
+	if err != nil {
+		fmt.Printf("Hash failed: %v\n", err)
+		return err
+	}
+
+	signature, err := csp.Sign(k, digest, signOpts)
 	if err != nil {
 		fmt.Printf("Sign failed: %v\n", err)
 		return err
@@ -65,7 +83,7 @@ func signAndVerify(csp cl.CSP, opts cl.KeyGenOpts) error {
 	fmt.Println("Public Key: ", hex.EncodeToString(kd))
 	fmt.Println("Signature: ", hex.EncodeToString(signature))
 
-	valid, err := csp.Verify(pubKey, digest, signature)
+	valid, err := csp.Verify(pubKey, digest, signature, signOpts)
 	if err != nil {
 		fmt.Printf("Verify failed: %v\n", err)
 		return err
--- pkgs/cryptolib/go.mod
+++ pkgs/cryptolib/go.mod
@@ -2,4 +2,10 @@ module github.com/ewangplay/golang-exercises/pkgs/cryptolib
 
 go 1.14
 
-require github.com/ewangplay/cryptolib v0.3.0
+require (
+	github.com/btcsuite/btcd v0.22.0-beta // indirect
+	github.com/ethereum/go-ethereum v1.10.12 // indirect
+	github.com/ewangplay/cryptolib v0.6.0
+	golang.org/x/crypto v0.0.0-20211115234514-b4de73f9ece8 // indirect
+	golang.org/x/sys v0.0.0-20211116061358-0a5406a5449c // indirect
+)
